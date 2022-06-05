5fb63363d2fef5679ae51aecd213a80b9637b265
slightair <arksutite@gmail.com>
2022-06-05 22:27:32 +0900

    Introduce Firestore

--- Pancake/PancakeApp.swift
+++ Pancake/PancakeApp.swift
@@ -1,8 +1,20 @@
 import ComposableArchitecture
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_: UIApplication,
+                     didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+
+        return true
+    }
+}
 
 @main
 struct PancakeApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             AppView(
